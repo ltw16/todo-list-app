a76fa80bffaaa022549ab6b9fa44bfba42664227
Liam Webb <liamwebb11@hotmail.com>
2018-09-15 11:17:41 +0000

    fix attempt 2

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -4,7 +4,7 @@ import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
 export const getItems = () => dispatch => {
     dispatch(setItemsLoading());
     axios  
-        .get("https://mern-stack-app-liamwebb.c9users.io:8081/api/items")
+        .get("/api/items")
         .then(res => 
             dispatch({
                 type: GET_ITEMS,
@@ -13,7 +13,7 @@ export const getItems = () => dispatch => {
 };
 
 export const deleteItem = (id) => dispatch => {
-    axios.delete(`https://mern-stack-app-liamwebb.c9users.io:8081/api/items/${id}`).then(res => 
+    axios.delete(`/api/items/${id}`).then(res => 
         dispatch({
             type: DELETE_ITEM,
             payload: id
@@ -23,7 +23,7 @@ export const deleteItem = (id) => dispatch => {
 
 export const addItem = (item) => dispatch => {
     axios
-        .post("https://mern-stack-app-liamwebb.c9users.io:8081/api/items", item)
+        .post("/api/items", item)
         .then(res => 
             dispatch({
                 type: ADD_ITEM,
